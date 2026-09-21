8bb669de4907ede09706fee29c4bb730ad39b0a8
Adithya Anil <adithyaanilpayyanur3@gmail.com>
2026-09-21 09:59:13 +0530

    Add Python Class Programs

---
+++ fifth.py
@@ -0,0 +1,87 @@
+#  conditional statement
+# true/false
+
+# age = int(input("enter your age:"))
+
+# if age >= 18:
+#     print("eligible to vote")
+# else:
+#     print("not eligible")
+
+# check if a number is odd or even
+
+# num = int(input("enter a number:"))
+# if  num%2 ==0:
+#    print("even")
+# else:
+#    print("odd")
+
+
+#    find largest of 2 numbers
+
+# a=int(input("number:"))
+# b=int(input("number:"))
+
+# if a>b:
+#    print(a,"largest")
+# else:
+#    print(b,"largest")
+
+# area of a triangle
+
+# base=int(input("enter base:"))
+# height=int(input("enter height:"))
+# if base > 0 and height > 0:
+#     area=0.5* base* height
+#     print("Area of triangle:",area)
+# else:
+#     print("invalid input")
+
+# perimeter of a circle
+
+# radius =int(input("Enter radius:"))
+# if radius > 0:
+#     perimeter = 2 * 3.14 *radius
+#     print("Perimeter of circle:",perimeter)
+# else:
+#     print("Invalid radius")
+
+# square of a number
+
+# a = int(input("Enter number: "))
+# if a >= 0:
+#     print("Square:", a * a)
+# else:
+#     print("Number is negative")
+
+# converts dollar to inr
+
+# dollar = float(input("Enter dollar: "))
+# if dollar > 0:
+#     print("INR:", dollar * 88)
+# else:
+#     print("Invalid amount")
+
+#  miles to km
+
+# miles = float(input("Enter miles: "))
+# if miles >= 0:
+#     print("KM:", miles * 1.609)
+# else:
+#     print("Invalid distance")
+
+# check if a number is positive or negative
+
+# a = int(input("Enter number: "))
+# if a > 0:
+#     print("Positive")
+# else:
+#     print("Negative")
+
+#  check if a number is a multiple of 5
+
+# a = int(input("Enter number: "))
+# if a % 5 == 0:
+#     print("Multiple of 5")
+# else:
+#     print("Not a multiple of 5")
